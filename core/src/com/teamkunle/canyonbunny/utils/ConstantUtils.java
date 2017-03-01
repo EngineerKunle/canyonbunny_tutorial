bb17ea607abd3dc6133410fbdc8c4314cdbd2d23
Kunle Ogunjimi <kdotkid3@gmail.com>
2017-03-01 22:48:28 +0000

    build.gradle updated with relevant libraries added... Main class in desktop ready

--- core/src/com/teamkunle/canyonbunny/utils/ConstantUtils.java
+++ core/src/com/teamkunle/canyonbunny/utils/ConstantUtils.java
@@ -2,6 +2,16 @@ package com.teamkunle.canyonbunny.utils;
 
 public class ConstantUtils {
 
+	public static String GAME_TITLE = "Canyon-Bunny";
+	
 	public static float VIEWPORT_WIDTH = 5.0f;
 	public static float VIEWPORT_HEIGHT = 5.0f;
+	
+	/*Settings width and height*/
+	public static int SETTINGS_WIDTH = 1024;
+	public static int SETTINGS_HEIGHT = 1024;
+	
+	/*configuration height and width*/
+	public static int GLOBAL_WIDTH = 800;
+	public static int GLOBAL_HEIGHT = 480;
 }
--- desktop/src/com/teamkunle/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/teamkunle/canyonbunny/desktop/DesktopLauncher.java
@@ -2,11 +2,29 @@ package com.teamkunle.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.teamkunle.canyonbunny.Main;
+import com.teamkunle.canyonbunny.utils.ConstantUtils;
 
 public class DesktopLauncher {
+	private static boolean reBuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
 	public static void main (String[] arg) {
+		if (reBuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = ConstantUtils.SETTINGS_WIDTH;
+			settings.maxHeight = ConstantUtils.SETTINGS_HEIGHT;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images",
+					"../canyonbunny-android/assets/images","canyonbunny.pack");
+		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = ConstantUtils.GAME_TITLE;
+		config.width = ConstantUtils.GLOBAL_WIDTH;
+		config.height = ConstantUtils.GLOBAL_HEIGHT;
 		new LwjglApplication(new Main(), config);
 	}
 }
